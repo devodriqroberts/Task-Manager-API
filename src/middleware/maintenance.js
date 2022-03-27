dbd11f2bbe5d76495bb9c46a86655066c8d6b337
Devodriq Roberts <devodriqroberts@Devodriqs-Mac-mini.local>
2022-03-26 20:45:46 -0400

    update url

--- src/middleware/maintenance.js
+++ src/middleware/maintenance.js
@@ -1,7 +1,7 @@
 
 
 const maintenance_mode = (req, res, next) => {
-    res.status(503).send("Service is currently under maintenance. Check back soon.")
+    res.status(503).send("Service is currently under maintenance. Check back soon!")
 }
 
 export default maintenance_mode
